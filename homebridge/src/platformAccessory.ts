51131fa69b3b6ec4dd6667ad6496887b3feb3f8d
David Chavez <david@dcvz.io>
2021-01-10 18:10:22 +0100

    Add initial homebridge plugin

---
+++ homebridge/src/platformAccessory.ts
@@ -0,0 +1,94 @@
+import { Service, PlatformAccessory, CharacteristicGetCallback } from 'homebridge';
+
+import { OdradekHomebridgePlatform } from './platform';
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class OdradekPlatformAccessory {
+  private service: Service;
+
+  /**
+   * These are just used to create a working example
+   * You should implement your own code to track the state of your accessory
+   */
+  private exampleStates = {
+    On: false,
+  };
+
+  constructor(
+    private readonly platform: OdradekHomebridgePlatform,
+    private readonly accessory: PlatformAccessory,
+  ) {
+
+    // set accessory information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Dreamy Kitties')
+      .setCharacteristic(this.platform.Characteristic.Model, accessory.context.device.uniqueId)
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+
+    // get the LightBulb service if it exists, otherwise create a new LightBulb service
+    // you can create multiple services for each accessory
+    this.service = this.accessory.getService(this.platform.Service.ContactSensor)
+    || this.accessory.addService(this.platform.Service.ContactSensor);
+
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.displayName);
+
+    // each service must implement at-minimum the "required characteristics" for the given service type
+    // see https://developers.homebridge.io/#/service/Lightbulb
+
+    // register handlers for the On/Off Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.ContactSensorState)
+      .on('get', this.getState.bind(this));               // GET - bind to the `getOn` method below
+
+    /**
+     * Updating characteristics values asynchronously.
+     * 
+     * Example showing how to update the state of a Characteristic asynchronously instead
+     * of using the `on('get')` handlers.
+     * Here we change update the motion sensor trigger states on and off every 10 seconds
+     * the `updateCharacteristic` method.
+     * 
+     */
+    let motionDetected = false;
+    setInterval(() => {
+      // EXAMPLE - inverse the trigger
+      motionDetected = !motionDetected;
+
+      // push the new value to HomeKit
+      this.service.updateCharacteristic(this.platform.Characteristic.ContactSensorState, motionDetected);
+
+      this.platform.log.debug('Triggering service:', !motionDetected);
+    }, 10000);
+  }
+
+  /**
+   * Handle the "GET" requests from HomeKit
+   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
+   * 
+   * GET requests should return as fast as possbile. A long delay here will result in
+   * HomeKit being unresponsive and a bad user experience in general.
+   * 
+   * If your device takes time to respond you should update the status of your device
+   * asynchronously instead using the `updateCharacteristic` method instead.
+
+   * @example
+   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+   */
+  getState(callback: CharacteristicGetCallback) {
+
+    // implement your own code to check if the device is on
+    const isOn = this.exampleStates.On;
+
+    this.platform.log.debug('Get Characteristic On ->', isOn);
+
+    // you must call the callback function
+    // the first argument should be null if there were no errors
+    // the second argument should be the value to return
+    callback(null, isOn);
+  }
+}
